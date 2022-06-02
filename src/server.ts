bb875d8ffbfbb8e4f3e24d8502de287f5aed19f5
Washington Mendes <washington.mendes@zigpay.com.br>
2022-06-02 12:39:47 -0300

    Ajuste para dowload do pdf no servidor

--- src/server.ts
+++ src/server.ts
@@ -34,8 +34,7 @@ app.get('/pdf', async (request, response) => {
             waitUntil: 'networkidle0'
         })
         const pdf = await page.pdf({
-            printBackground: true,
-            format: 'Letter'
+            path: path.join(__dirname, `../temp/pdf/${"id_5_30_05_2022_22" + ".pdf"}`), format: 'a4'
         })
 
         await browser.close()
@@ -43,50 +42,6 @@ app.get('/pdf', async (request, response) => {
         response.contentType("application/pdf")
 
         return response.send(pdf)
-
-        // Create the chart    
-        /*  Highcharts.chart({
-             chart: {
-                 type: 'column'
-             },
-             title: {
-                 text: 'HISTÓRICO DE NOTIFICAÇÕES'
-             },
-             subtitle: {
-                 text: 'ENVIADAS PARA O USUÁRIO'
-             },
-             xAxis: {
-                 categories: ['14/03/2022', '15/03/2022', '16/03/2022', '17/03/2022']
-             },
-             yAxis: {
-                 min: 0,
-                 title: {
-                     text: 'Qtd de notificações enviadas'
-                 }
-             },
-             tooltip: {
-                 pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
-                 shared: true,
-                 footerFormat: '<br/><b>Total:  {point.total} </b>'
-             },
-             plotOptions: {
-                 column: {
-                     stacking: 'number'
-                 }
-             },
-             series: [{
-                 name: 'WhatsApp',
-                 data: [5, 3, 4, 7]
-             }, {
-                 name: 'App',
-                 data: [5, 5, 4, 3]
-             },
-             {
-                 name: 'SMS',
-                 data: [2, 2, 3, 2]
-             }]
-         }); */
-        return response.send("Finish")
     } catch (error) {
 
     }
