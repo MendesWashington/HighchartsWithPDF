985aeaf11ee75bd79fe095a7e41aef1c11f83e7c
MendessWashington <mendesswashington@gmail.com>
2022-06-13 23:35:19 -0300

    funcionamento do download do pdf estilizado

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 
 
 import ejs from "ejs";
+import pdf from "html-pdf"
 import path from "path";
 import puppeteer from "puppeteer";
 import express from "express";
-import Highcharts from "highcharts";
+const exporter = require('highcharts-export-server');
 
 const app = express();
 const port = 3000;
@@ -12,17 +13,63 @@ const port = 3000;
 app.use(express.json())
 
 app.get('/', (request, response) => {
-
-    const filePath = path.join(__dirname, "print.ejs")
-    ejs.renderFile(filePath, (err, html) => {
-        if (err) {
-            return response.send('Erro na leitura do arquivo')
+    var exportSettings = {
+        type: 'png',
+        options: {
+            title: {
+                text: 'My Chart'
+            },
+            xAxis: {
+                categories: ["Jan", "Feb", "Mar", "Apr", "Mar", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            },
+            series: [
+                {
+                    type: 'line',
+                    data: [1, 3, 2, 4]
+                },
+                {
+                    type: 'line',
+                    data: [5, 3, 4, 2]
+                }
+            ]
         }
+    };
 
-        // enviar para o navegador
-        return response.send(html)
-    })
+    //Set up a pool of PhantomJS workers
+    exporter.initPool();
 
+    //Perform an export
+    /*
+        Export settings corresponds to the available CLI arguments described
+        above.
+    */
+    exporter.export(exportSettings, function (err: any, res: any) {
+        if (err) {
+            console.log(err)
+        }
+        console.log(res)
+
+        exporter.killPool();
+        process.exit(1);
+    });
+    /*   const filePath = path.join(__dirname, "print.ejs")
+      ejs.renderFile(filePath, (err, html) => {
+          if (err) {
+              return response.send('Erro na leitura do arquivo')
+          }
+          pdf.create(html, {}).toFile('./temp/pdf/newPdf.pdf', (err, res) => {
+              if (err) {
+                  return response.status(400).send({
+                      error: err.message
+                  });
+                  return;
+              }
+  
+              return response.status(200).send(html);
+          })
+          return;
+      })
+   */
 })
 app.get('/pdf', async (request, response) => {
 
@@ -30,11 +77,23 @@ app.get('/pdf', async (request, response) => {
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
 
+        const dataCreate = new Date()
+            .toLocaleDateString("pt-BR", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            })
+            .replace(/\//g, "_")
+            .replace(/:/g, "_")
+
         await page.goto('http://localhost:3000/', {
             waitUntil: 'networkidle0'
         })
         const pdf = await page.pdf({
-            path: path.join(__dirname, `../temp/pdf/${"id_5_30_05_2022_22" + ".pdf"}`), format: 'a4'
+            path: path.join(__dirname, `../temp/pdf/${"id_5_" + dataCreate + ".pdf"}`), format: 'a4'
         })
 
         await browser.close()
