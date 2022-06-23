22d79c7d7518a3463d925103cc50fdb925529ebd
MendessWashington <mendesswashington@gmail.com>
2022-06-23 13:44:59 -0300

    Adicionadochamada a api export chart

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import pdf from "html-pdf"
 import path from "path";
 import puppeteer from "puppeteer";
 import express from "express";
+import fs from "fs";
+import axios from "axios"
 const exporter = require('highcharts-export-server');
 
 const app = express();
@@ -12,9 +14,11 @@ const port = 3000;
 
 app.use(express.json())
 
-app.get('/', (request, response) => {
+
+function exportChart() {
+    //Export settings
     var exportSettings = {
-        type: 'png',
+        type: 'svg',
         options: {
             title: {
                 text: 'My Chart'
@@ -35,42 +39,104 @@ app.get('/', (request, response) => {
         }
     };
 
-    //Set up a pool of PhantomJS workers
+    //Configurar um pool de trabalhadores do PhantomJS
     exporter.initPool();
 
-    //Perform an export
+    //Faça uma exportação
     /*
-        Export settings corresponds to the available CLI arguments described
-        above.
+       As configurações de exportação correspondem aos argumentos CLI disponíveis descritos
+        acima de.
     */
     exporter.export(exportSettings, function (err: any, res: any) {
-        if (err) {
-            console.log(err)
-        }
-        console.log(res)
+        //O resultado da exportação está agora em res.
+        //Se a saída não for PDF ou SVG, ela será codificada em base64 (res.data).
+        //Se a saída for um PDF ou SVG, ela conterá um nome de arquivo (res.filename).
 
+        //Mate o pool quando terminarmos com ele e saia do aplicativo
         exporter.killPool();
         process.exit(1);
     });
-    /*   const filePath = path.join(__dirname, "print.ejs")
-      ejs.renderFile(filePath, (err, html) => {
-          if (err) {
-              return response.send('Erro na leitura do arquivo')
-          }
-          pdf.create(html, {}).toFile('./temp/pdf/newPdf.pdf', (err, res) => {
-              if (err) {
-                  return response.status(400).send({
-                      error: err.message
-                  });
-                  return;
-              }
-  
-              return response.status(200).send(html);
-          })
-          return;
-      })
-   */
+}
+
+async function postChart() {
+    const response = await axios.post('https://export.highcharts.com', {
+        infile: {
+            chart: {
+                type: "column"
+            },
+            credits: {
+                enabled: true,
+                text: "appsupply",
+                href: "https://painel.appsupply.ml/"
+            },
+            title: {
+                text: "HISTÓRICO DE NOTIFICAÇÕES"
+            },
+            subtitle: {
+                text: "ENVIADAS PARA O USUÁRIO"
+            },
+            xAxis: {
+                categories: ["14/03/2022", "15/03/2022", "16/03/2022", "17/03/2022"]
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: "Qtd de notificações enviadas"
+                }
+            },
+            tooltip: {
+                shared: true,
+                footerFormat: "<br/><b>Total:  {point.total} </b>"
+            },
+            plotOptions: {
+                column: {
+                    stacking: "number"
+                }
+            },
+            series: [
+                {
+                    name: "WhatsApp",
+                    data: [5, 3, 4, 7]
+                },
+                {
+                    name: "App",
+                    data: [5, 5, 4, 3]
+                },
+                {
+                    name: "SMS",
+                    data: [2, 2, 3, 2]
+                }
+            ]
+        }
+    })
+    return response;
+
+}
+
+app.get('/', (request, response) => {
+    // exportChart();
+    console.log(postChart())
+    return response.status(200).send("<img src='' /> ");
+    const filePath = path.join(__dirname, "print.html")
+    ejs.renderFile(filePath, (err, html) => {
+        if (err) {
+            return response.send('Erro na leitura do arquivo')
+        }
+        pdf.create(html, {}).toFile('./temp/pdf/newPdf.pdf', (err, res) => {
+            if (err) {
+                return response.status(400).send({
+                    error: err.message
+                });
+                return;
+            }
+
+            return response.status(200).send(html);
+        })
+        return;
+    })
+
 })
+
 app.get('/pdf', async (request, response) => {
 
     try {
